67f27a29636e21a42206944602fdc2707967b53c
ChrisT-CC <cristaecc@gmail.com>
2021-08-28 18:08:01 +0000

    Wire up API's and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Import the entire "gspread" library
+# So I can access any function, class or method within it
+import gspread
+# Import the "Credentials" class which is part of the "service_account"
+# function from the "google.oauth2" library
+from google.oauth2.service_account import Credentials
+
+# Set the scope
+# The scope lists the API's that the program should acces in order to run
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+# Create another constant variable named "CREDS"
+# Call the "from_service_account_file" method of the "Credentials" class
+# and pass it "creds.json"
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("love_sandwiches")
+
+sales = SHEET.worksheet("sales")
+
+data = sales.get_all_values()
+
+print(data)
